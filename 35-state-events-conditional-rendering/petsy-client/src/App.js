00079e2c4c6b6d9c4cd7f2eb458d2d52733043db
sbal13 <sbal13@gmail.com>
2019-06-04 11:27:35 -0400

    pre state

---
+++ 35-state-events-conditional-rendering/petsy-client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import categories from './data'
+
+function App() {
+  console.log(categories)
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
